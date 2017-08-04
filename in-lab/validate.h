79a26a13613172041e1d6c3c465670e23a796616
ryan.wang23 <ryan.wang23@hotmail.com>
2017-08-04 17:18:32 -0400

    Done

--- in-lab/validate.h
+++ in-lab/validate.h
@@ -1,22 +1,52 @@
+//#ifndef ICT_VALIDATE_H__
+//#define ICT_VALIDATE_H__
+//#include <iostream>
+//
+//namespace ict {
+//    template <typename vali>
+//    bool validate(const vali& minimum, const vali& maximum, const vali* value, int elements, bool* bools) {
+//        for (int i = 0; i < elements; i++) {
+//            if (value[i] >= minimum && value[i] <= maximum) {
+//                bools[i] = true;
+//            }
+//            else {
+//                bools[i] = false;
+//                //valid = false;
+//                return 0;
+//            }
+//        }
+//
+//        return 1;
+//    }
+//}
+//
+//
+//#endif // !SICT_VALIDATE_H__
+
 #ifndef ICT_VALIDATE_H__
 #define ICT_VALIDATE_H__
 #include <iostream>
 
 namespace ict {
-    template <typename vali>
-    bool validate(const vali& minimum, const vali& maximum, const vali* value, int elements, bool* bools) {
-        for (int i = 0; i < elements; i++) {
-            if (value[i] >= minimum && value[i] <= maximum) {
-                bools[i] = true;
+    template <typename T>
+    bool validate(const T& min, const T& max, const T* testValue, int n, bool* B) {
+        bool valid = true;
+        for (int i = 0; i < n; i++) {
+            if (testValue[i] >= min && testValue[i] <= max) {
+                B[i] = true;
             }
             else {
-                bools[i] = false;
-                //valid = false;
-                return 0;
+                B[i] = false;
+                valid = false;
             }
         }
 
-        return 1;
+        //for (int j = 0; j < n; j++) {
+        //  if (B[j] = false) {
+        //    valid = false;
+        //  }
+        //}
+        return valid;
     }
 }
 
